52e021320f6a2def7631959b68125cd5d55695e9
Luan Henrique <luanhenriquens@hotmail.com>
2020-09-12 20:59:08 -0300

    Entities Group and MemberGroup

--- Data/Models/Group.cs
+++ Data/Models/Group.cs
@@ -2,14 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace escala_server.Data.Models
 {
-    public class MemberFunction
+    public class Group
     {
         [Key]
         public int Id { get; set; }
 
-        public int MemberId { get; set; }
-
-        public int FunctionId { get; set; }
+        public string Name { get; set; }
 
         public bool Active { get; set; }
     }
--- Data/Models/MemberGroup.cs
+++ Data/Models/MemberGroup.cs
@@ -2,14 +2,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace escala_server.Data.Models
 {
-    public class MemberFunction
+    public class MemberGroup
     {
         [Key]
         public int Id { get; set; }
 
         public int MemberId { get; set; }
 
-        public int FunctionId { get; set; }
+        public int GroupId { get; set; }
+
+        public bool Admin { get; set; }
 
         public bool Active { get; set; }
     }
